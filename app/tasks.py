c7fb8fdf946f1e0cd002186f1bff8846d62421ac
听听那冷雨 <fuyunliu@users.noreply.github.com>
2019-04-12 11:32:08 +0800

    Update tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from threading import Thread
 from flask import current_app, render_template
 from flask_mail import Message
-from . import mail, celery
+from celery import shared_task
+from . import mail
 
 
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
+@shared_task(serializer='pickle')
+def send_async_email(msg):
+    mail.send(msg)
 
 
 def send_email(to, subject, template, **kwargs):
@@ -17,6 +17,4 @@ def send_email(to, subject, template, **kwargs):
                   sender=app.config['MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=(app, msg))
-    thr.start()
-    return thr
+    send_async_email.delay(msg)
