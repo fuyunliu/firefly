e8505cfd5099321edac32968ce77cd211c86ffb0
听听那冷雨 <fuyunliu@users.noreply.github.com>
2019-04-12 11:38:27 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ db = SQLAlchemy()
 mail = Mail()
 celery = Celery(__name__)
 celery.config_from_object(Config, namespace='CELERY')
-celery.autodiscover_tasks(['app.tasks'])
+celery.autodiscover_tasks(['app.email'])
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 
